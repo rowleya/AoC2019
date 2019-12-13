ffedab5a0474147de33966140be1de532446dbe2
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-12-13 08:40:11 +0000

    Day 13 part 2 done

--- src/test/ArcadeIntCode.java
+++ src/test/ArcadeIntCode.java
@@ -51,12 +51,13 @@ class Instruction6 {
 
     public long getValue(long[] prog, int pc, int arg, int relBase) {
         int pos = getPos(prog, pc, arg, relBase);
-        System.err.println("Reading from " + pos);
         return prog[pos];
     }
 }
 
 class IntCodeMachine6 {
+
+    private static final boolean DEBUG = false;
 
     private long[] prog;
 
@@ -68,6 +69,12 @@ class IntCodeMachine6 {
 
     private int relBase = 0;
 
+    private static final void println(String s) {
+        if (DEBUG) {
+            System.err.println(s);
+        }
+    }
+
     public IntCodeMachine6(long[] origProg, int size) {
         prog = new long[size];
         for (int i = 0; i < origProg.length; i++) {
@@ -77,11 +84,13 @@ class IntCodeMachine6 {
     }
 
     private void printInstruction(int nArgs) {
-        System.err.print(pc + ": " + prog[pc]);
-        for (int i = 0; i < nArgs; i++) {
-            System.err.print(" " + prog[pc + i + 1]);
+        if (DEBUG) {
+            System.err.print(pc + ": " + prog[pc]);
+            for (int i = 0; i < nArgs; i++) {
+                System.err.print(" " + prog[pc + i + 1]);
+            }
+            System.err.println();
         }
-        System.err.println();
     }
 
     public boolean isFinished() {
@@ -105,7 +114,7 @@ class IntCodeMachine6 {
                     long insum1 = i.getValue(prog, pc, 0, relBase);
                     long  insum2 = i.getValue(prog, pc, 1, relBase);
                     prog[outsum] = insum1 + insum2;
-                    System.err.println("pc[" + outsum + "] = " + insum1 + " + " + insum2);
+                    println("pc[" + outsum + "] = " + insum1 + " + " + insum2);
                     pc += 4;
                     break;
 
@@ -115,7 +124,7 @@ class IntCodeMachine6 {
                     long inmul1 = i.getValue(prog, pc, 0, relBase);
                     long inmul2 = i.getValue(prog, pc, 1, relBase);
                     prog[outmul] = inmul1 * inmul2;
-                    System.err.println("pc[" + outmul + "] = " + inmul1 + " * " + inmul2);
+                    println("pc[" + outmul + "] = " + inmul1 + " * " + inmul2);
                     pc += 4;
                     break;
 
@@ -124,22 +133,22 @@ class IntCodeMachine6 {
                     long strval = -1;
                     if (inputIter.hasNext()) {
                         strval = inputIter.next();
-                        System.err.println("Using " + strval + " as input");
+                        println("Using " + strval + " as input");
                     } else {
-                        System.err.println("Out of input");
+                        println("Out of input");
                         outOfInput = true;
                         break;
                     }
                     int outstr = i.getPos(prog, pc, 0, relBase);
                     prog[outstr] = strval;
-                    System.err.println("pc[" + outstr + "] = " + strval);
+                    println("pc[" + outstr + "] = " + strval);
                     pc += 2;
                     break;
 
                 case 4:
                     printInstruction(1);
                     long ldr = i.getValue(prog, pc, 0, relBase);
-                    System.err.println("Output: " + ldr);
+                    println("Output: " + ldr);
                     lastOutput = ldr;
                     outputs.add(ldr);
                     pc += 2;
@@ -150,10 +159,10 @@ class IntCodeMachine6 {
                     long valueTrue = i.getValue(prog, pc, 0, relBase);
                     int jumpTrue = (int) i.getValue(prog, pc, 1, relBase);
                     if (valueTrue > 0) {
-                        System.err.println("Jump to " + jumpTrue + " because " + valueTrue + " > 0");
+                        println("Jump to " + jumpTrue + " because " + valueTrue + " > 0");
                         pc = jumpTrue;
                     } else {
-                        System.err.println("Don't Jump to " + jumpTrue + " because " + valueTrue + " <= 0");
+                        println("Don't Jump to " + jumpTrue + " because " + valueTrue + " <= 0");
                         pc += 3;
                     }
                     break;
@@ -162,11 +171,11 @@ class IntCodeMachine6 {
                     printInstruction(2);
                     long valueFalse = i.getValue(prog, pc, 0, relBase);
                     int jumpFalse = (int) i.getValue(prog, pc, 1, relBase);
-                    System.err.println("Jump to " + jumpFalse + " because " + valueFalse + " == 0");
                     if (valueFalse == 0) {
+                        println("Jump to " + jumpFalse + " because " + valueFalse + " == 0");
                         pc = jumpFalse;
                     } else {
-                        System.err.println("Don't Jump to " + jumpFalse + " because " + valueFalse + " != 0");
+                        println("Don't Jump to " + jumpFalse + " because " + valueFalse + " != 0");
                         pc += 3;
                     }
                     break;
@@ -177,10 +186,10 @@ class IntCodeMachine6 {
                     long ltv2 = i.getValue(prog, pc, 1, relBase);
                     int ltout = i.getPos(prog, pc, 2, relBase);
                     if (ltv1 < ltv2) {
-                        System.err.println("prog[" + ltout + "] = 1 (" + ltv1 + " < " + ltv2 + ")");
+                        println("prog[" + ltout + "] = 1 (" + ltv1 + " < " + ltv2 + ")");
                         prog[ltout] = 1;
                     } else {
-                        System.err.println("prog[" + ltout + "] = 0 (" + ltv1 + " >= " + ltv2 + ")");
+                        println("prog[" + ltout + "] = 0 (" + ltv1 + " >= " + ltv2 + ")");
                         prog[ltout] = 0;
                     }
                     pc += 4;
@@ -192,10 +201,10 @@ class IntCodeMachine6 {
                     long eqv2 = i.getValue(prog, pc, 1, relBase);
                     int eqout = i.getPos(prog, pc, 2, relBase);
                     if (eqv1 == eqv2) {
-                        System.err.println("prog[" + eqout + "] = 1 (" + eqv1 + " == " + eqv2 + ")");
+                        println("prog[" + eqout + "] = 1 (" + eqv1 + " == " + eqv2 + ")");
                         prog[eqout] = 1;
                     } else {
-                        System.err.println("prog[" + eqout + "] = 0 (" + eqv1 + " != " + eqv2 + ")");
+                        println("prog[" + eqout + "] = 0 (" + eqv1 + " != " + eqv2 + ")");
                         prog[eqout] = 0;
                     }
                     pc += 4;
@@ -205,14 +214,14 @@ class IntCodeMachine6 {
                     printInstruction(1);
                     int newBase = (int) i.getValue(prog, pc, 0, relBase);
                     relBase += newBase;
-                    System.err.println("relBase += " + newBase + " = " + relBase);
+                    println("relBase += " + newBase + " = " + relBase);
                     pc += 2;
                     break;
 
                 case 99:
                     printInstruction(0);
                     finished = true;
-                    System.err.println("Finished!");
+                    println("Finished!");
                     break;
 
                 default:
@@ -224,89 +233,89 @@ class IntCodeMachine6 {
 }
 
 class Pos {
-	int x;
-	int y;
-	
-	public Pos(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	@Override
-	public int hashCode() {
-		return (x + "," + y).hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Pos) {
-			Pos c = (Pos) obj;
-			return c.x == x && c.y == y;
-		}
-		return false;
-	}
+    int x;
+    int y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return (x + "," + y).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Pos) {
+            Pos c = (Pos) obj;
+            return c.x == x && c.y == y;
+        }
+        return false;
+    }
 }
 
 class Tile {
-	public int x;
-	public int y;
-	public int type;
-	
-	public Tile(int x, int y, int type) {
-		this.x = x;
-		this.y = y;
-		this.type = type;
-	}
-	
-	@Override
-	public int hashCode() {
-		return (x + "," + y).hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Tile) {
-			Tile c = (Tile) obj;
-			return c.x == x && c.y == y;
-		}
-		return false;
-	}
+    public int x;
+    public int y;
+    public int type;
+
+    public Tile(int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        return (x + "," + y).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Tile) {
+            Tile c = (Tile) obj;
+            return c.x == x && c.y == y;
+        }
+        return false;
+    }
 }
 
 public class ArcadeIntCode {
-	
-	public static void draw(Map<Pos, Tile> tiles, int minX, int minY, int maxX, int maxY) {
-		for (int i = minY; i <= maxY; i++) {
-			for (int j = minX; j <= maxX; j++) {
-				Pos p = new Pos(j, i);
-				if (!tiles.containsKey(p)) {
-					System.err.print(' ');
-				} else {
-					Tile t = tiles.get(p);
-					char c = ' ';
-					switch (t.type) {
-					case 0:
-						c = ' ';
-						break;
-					case 1:
-						c = '#';
-						break;
-					case 2:
-						c = '*';
-						break;
-					case 3:
-						c = '-';
-						break;
-					case 4:
-						c = '0';
-						break;
-					}
-					System.err.print(c);;
-				}
-			}
-			System.err.println();
-		}
-	}
+
+    public static void draw(Map<Pos, Tile> tiles, int minX, int minY, int maxX, int maxY) {
+        for (int i = minY; i <= maxY; i++) {
+            for (int j = minX; j <= maxX; j++) {
+                Pos p = new Pos(j, i);
+                if (!tiles.containsKey(p)) {
+                    System.err.print(' ');
+                } else {
+                    Tile t = tiles.get(p);
+                    char c = ' ';
+                    switch (t.type) {
+                    case 0:
+                        c = ' ';
+                        break;
+                    case 1:
+                        c = '#';
+                        break;
+                    case 2:
+                        c = '*';
+                        break;
+                    case 3:
+                        c = '-';
+                        break;
+                    case 4:
+                        c = '0';
+                        break;
+                    }
+                    System.err.print(c);;
+                }
+            }
+            System.err.println();
+        }
+    }
 
     public static void main(String[] args) throws Exception {
 
@@ -319,11 +328,11 @@ public class ArcadeIntCode {
             prog[i] = Long.parseLong(progString[i]);
         }
         prog[0] = 2;
-        
+
         Map<Pos, Tile> tiles = new HashMap<Pos, Tile>();
-        
+
         IntCodeMachine6 m = new IntCodeMachine6(prog, 1000000);
-        
+
         int score = 0;
         int minX = Integer.MAX_VALUE;
         int maxX = Integer.MIN_VALUE;
@@ -331,44 +340,50 @@ public class ArcadeIntCode {
         int maxY = Integer.MIN_VALUE;
         int[] inputs = new int[]{};
         Scanner in = new Scanner(System.in);
-        int nBlocks = 1;
-        while (!m.isFinished() && nBlocks > 0) {
-        	int ballX = 0;
-        	int paddleX = 0;
-        	nBlocks = 0;
+        Set<Pos> blocks = new HashSet<Pos>();
+        while (!m.isFinished()) {
+            int ballX = 0;
+            int paddleX = 0;
             List<Long> outputs = m.runUntilOutOfInputOrFinished(inputs);
             for (int i = 0; i < outputs.size(); i += 3) {
-            	int x = outputs.get(i).intValue();
-            	int y = outputs.get(i + 1).intValue();
-            	int type = outputs.get(i + 2).intValue();
-            	if (x == -1 && y == 0) {
-            		score = type;
-            	} else {
-            		maxX = Math.max(x, maxX);
-            		maxY = Math.max(y, maxY);
-            		minX = Math.min(x, minX);
-            		minY = Math.min(y, minY);
-		        	
-		        	Tile t = new Tile(x, y, type);
-		        	Pos p = new Pos(x, y);
-		        	tiles.put(p, t);
-		        	if (type == 2) {
-		        		nBlocks++;
-		        	} else if (type == 4) {
-		        		ballX = x;
-		        	} else if (type == 3) {
-		        		paddleX = x;
-		        	}
-            	}
+                int x = outputs.get(i).intValue();
+                int y = outputs.get(i + 1).intValue();
+                int type = outputs.get(i + 2).intValue();
+                if (x == -1 && y == 0) {
+                    score = type;
+                } else {
+                    maxX = Math.max(x, maxX);
+                    maxY = Math.max(y, maxY);
+                    minX = Math.min(x, minX);
+                    minY = Math.min(y, minY);
+
+                    Tile t = new Tile(x, y, type);
+                    Pos p = new Pos(x, y);
+                    tiles.put(p, t);
+                    System.err.println(type);
+                    if (type == 2) {
+                        blocks.add(p);
+                    } else {
+                        blocks.remove(p);
+                        if (type == 4) {
+                            ballX = x;
+                        } else if (type == 3) {
+                            paddleX = x;
+                        }
+                    }
+                }
             }
-            System.err.println(nBlocks);
+            System.err.println(blocks.size());
             draw(tiles, minX, minY, maxX, maxY);
             System.err.println(score);
+            if (blocks.size() == 0) {
+                break;
+            }
             int inp = 0;
             if (ballX > paddleX) {
-            	inp = 1;
+                inp = 1;
             } else if (paddleX > ballX) {
-            	inp = -1;
+                inp = -1;
             }
             System.err.println("Joystick: " + inp);
             inputs = new int[] {inp};
